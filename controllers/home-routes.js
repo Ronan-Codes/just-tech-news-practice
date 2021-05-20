dd347594a5c27dfba2c5cb09cd96f633e0118d3a
Ronan <galv.ronan@gmail.com>
2021-05-19 21:02:51 -0400

    Added upvote functionality when logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,10 +32,14 @@ router.get('/', (req, res) => {
             ]
         })
         .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
+            const posts = dbPostData.map(post => post.get({
+                plain: true
+            }));
             // res.render('homepage', dbPostData[0].get({ plain: true }));
             // Add posts array to object instead and continue passing an the object to the template
-            res.render('homepage', { posts });
+            res.render('homepage', {
+                posts
+            });
         })
         .catch(err => {
             console.log(err);
@@ -46,12 +50,62 @@ router.get('/', (req, res) => {
 // route for login page. login.handlebars to main.handlebars, and then utilize login.js
 router.get('/login', (req, res) => {
     // check for a session and redirect to the homepage if one exists
-    if(req.session.loggedIn) {
+    if (req.session.loggedIn) {
         res.redirect('/');
         return;
     }
 
     res.render('login');
+});
+
+router.get('/posts/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'post_url',
+                'title',
+                'created_at',
+                [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({
+                    message: 'No post found with this id'
+                });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({
+                plain: true
+            });
+
+            // pass data to template
+            res.render('single-post', {
+                post
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
